5c92f4ede8b6cab1ef5ee86bc9062d0fb6e7d951
Dan Swain <dan.t.swain@gmail.com>
2021-03-17 20:28:47 -0400

    Working POC

--- go.mod
+++ go.mod
@@ -5,10 +5,11 @@ go 1.15
 require (
 	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.9.0
+	github.com/prometheus/common v0.15.0
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
 	gopkg.in/src-d/go-git.v4 v4.12.0
 	gotest.tools/gotestsum v0.6.0 // indirect
 )
--- internal/pkg/cli/root.go
+++ internal/pkg/cli/root.go
@@ -1,19 +1,62 @@
 package cli
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "prometheus_relay",
+	Use:   "prometheus_relay [prometheus_url] [post_to_url]",
 	Short: "Relay prometheus data",
 	Long:  `Relay prometheus data`,
+	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
-		os.Exit(1)
+		prometheusClientUrl := args[0]
+		postToUrl := args[1]
+
+		client, err := api.NewClient(api.Config{
+			Address: prometheusClientUrl,
+		})
+		if err != nil {
+			fmt.Printf("Error creating client: %v\n", err)
+			os.Exit(1)
+		}
+
+		v1api := v1.NewAPI(client)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		result, warnings, err := v1api.Query(ctx, "ambient_wx_temperature", time.Now())
+		if err != nil {
+			fmt.Printf("Error querying Prometheus: %v\n", err)
+			os.Exit(1)
+		}
+		if len(warnings) > 0 {
+			fmt.Printf("Warnings: %v\n", warnings)
+		}
+
+		url := postToUrl
+		for _, v := range result.(model.Vector) {
+			location := v.Metric[model.LabelName("location")]
+			value := v.Value
+
+			vals := map[string]interface{}{"name": location, "value": value}
+			str, _ := json.Marshal(vals)
+			_, err := http.Post(url, "application/json", bytes.NewBuffer(str))
+			if err != nil {
+				fmt.Printf("Error posting: %v\n", err)
+				os.Exit(1)
+			}
+		}
 	},
 }
 
